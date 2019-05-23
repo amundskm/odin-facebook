62eab5b58a4aca9f2f23c1cfbdcb294eac1f1c42
amundskm <amundskm@gmail.com>
2019-05-23 14:39:56 -0500

    correct users index to not show pending friends

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -7,5 +7,6 @@ class FriendRequestsController < ApplicationController
 
     def index
         @friend_requests = current_user.received_friend_requests
+        redirect_to friend_requests_path
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
   include ApplicationHelper
-  
+
   def index
     no_show = current_user.friends.ids
+    no_show += current_user.pending_friend_requests.pluck(:receiver_id)
     no_show << current_user.id
     @users = User.where.not(id: no_show)
     @friends = current_user.friends
